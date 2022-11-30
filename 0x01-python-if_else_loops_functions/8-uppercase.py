bd8521148dc861f51a8b7a95d2be13b52694eeaa
Lincon-snel <linconsnel@gmail.com>
2022-11-30 04:39:54 +0000

    Printing in uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def to_uper(character):
+    """Function converts a character to uppercase"""
+    if ord(character) >= 97 and ord(character) <= 122:
+        return (ord(character) - 32)
+    else:
+        return ord(character)
+
+
+def uppercase(string):
+    """Function prints a string in uppercase"""
+    string_new = ""
+    for character in string:
+        string_new += "%c" % to_uper(character)
+    print("{:s}".format(string_new))
