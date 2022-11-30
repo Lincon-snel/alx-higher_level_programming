ce7b67f62f56568ce1613c98911967ccd6d50f9f
Lincon-snel <linconsnel@gmail.com>
2022-11-30 01:08:24 +0000

    Finding last digit

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number < 0:
+    numberN = -1 * number
+    ld = numberN % 10
+    ld = -1 * ld
+else:
+    ld = number % 10
+
+
+if ld == 0:
+    print("Last digit of {} is {} and is 0".format(number, ld))
+elif ld > 5:
+    print("Last digit of {} is {} and is greater than 5".format(number, ld))
+else:
+    print("Last digit of {} is {} and is less than 6 and not 0".format(number, ld))
