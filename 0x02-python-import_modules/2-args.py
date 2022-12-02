ee68cc0c17f89a8c7896807099bae031d971e266
Lincon-snel <linconsnel@gmail.com>
2022-12-02 02:17:28 +0000

    Prints the num and list of arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Prints the number and list of its arguments."""
+    import sys
+    numb = len(sys.argv) - 1
+    if numb == 0:
+        print("0 arguments.")
+    elif numb == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(numb))
+    for i in range(numb):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
