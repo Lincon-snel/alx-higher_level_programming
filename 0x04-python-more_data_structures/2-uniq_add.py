2f0123ca341df7962f0014aff6f1d7672fd4690d
Lincon-snel <linconsnel@gmail.com>
2022-12-10 04:58:12 +0000

    Advance tasks and corrections

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    total = 0
-    for i in set(my_list):
-        result += i
-    return total
+    result = 0
+    for x in set(my_list):
+        result += x
+    return (result)
