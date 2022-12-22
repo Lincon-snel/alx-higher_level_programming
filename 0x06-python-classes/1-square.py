134f8efc37f6c802e7d928c0799a3f1173408b7f
Lincon-snel <linconsnel@gmail.com>
2022-12-22 04:32:10 +0000

    Uploading mandatory tasks

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Defines a suqare.
+"""
+
+
+class Square:
+    """ Square class """
+    def __init__(self, size):
+        """ Class initialization """
+        self.__size = size
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+This is for the ALX 0x06-python-classes project under Python programming
